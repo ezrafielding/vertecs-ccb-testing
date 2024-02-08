dace97a27d446664f546df1eb3f37916d730df8c
Ezra Fielding <fielding.ezra455@mail.kyutech.jp>
2024-02-08 13:18:51 +0900

    Made rts UART test more readable

--- ccb/rtscts_uart.py
+++ ccb/rtscts_uart.py
@@ -7,12 +7,18 @@ def sync():
 
 # Serial port configuration:
 serPort = serial.Serial("/dev/ttyUSB0", 12000000, timeout=None, rtscts=True, dsrdtr=False)
-
+print('Serial Port Open!')
 firstTime = True
 with open('./test.png', 'rb') as img_file:
+    print('Reading File...')
     img_data = img_file.read()
+
+print('Begin Data Transfer...')
 sync()
 serPort.write(img_data)
+print('Transfer Complete!')
 
 serPort.flush()
 serPort.close()
+print('Serial Port Closed.')
+print('Done!')
